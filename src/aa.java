076392f0ba748c6efbd65711a42180baa5757478
l1163080928 <1163080928@qq.com>
2023-10-13 17:33:05 +0800

    Initial commit

---
+++ src/aa.java
@@ -0,0 +1,10 @@
+/**
+ * @author ly
+ * @data 2023/10/13 17:27
+ * @describe
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
